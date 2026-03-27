beecf88843ad0678ff46d7e558d9de1154451d04
PocketCorporation <73258409+PocketCorporation@users.noreply.github.com>
2026-03-27 21:36:28 +0000

    Beginning dropdown and items

---
+++ script.js
@@ -0,0 +1,9 @@
+const showSidebar=(toggleId,sidebarId)=>{
+    const toggle = document.getElementById(toggleId),
+        sidebar = document.getElementById(sidebarId)
+
+    toggle.addEventListener('click',()=>{
+        sidebar.classList.toggle('show-sidebar')
+    })
+}
+showSidebar('header-toggle','sidebar')
